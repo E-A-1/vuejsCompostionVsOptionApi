ba563d96dd9601fcf2f74a71d416d1e2fde47cea
EAVijay <eavijay111@gmail.com>
2022-12-12 14:47:54 +0530

    heightCheck Page

---
+++ src/views/minHeightCheck.vue
@@ -0,0 +1,23 @@
+<template>
+    <div style="height:50px;background-color:yellow"></div>
+    <div style="min-height:calc(100vh - 100px);background-color:green">
+
+        <div style="height:300px;border:2px solid red"></div>
+        <div style="height:300px;border:2px solid red"></div>
+        <div style="height:300px;border:2px solid red"></div>
+        <div style="height:300px;border:2px solid red"></div>
+    </div>
+    <div style="height:50px;background-color:red;position:sticky"></div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        //
+    },
+})
+</script>
+<style scoped>
+
+</style>
